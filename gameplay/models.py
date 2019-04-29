a47cd9111355040e221a58945a39dfe4fe8e39d2
utima <uta_m@freenet.de>
2019-04-29 17:04:28 +0200

    editable list games admin

--- gameplay/models.py
+++ gameplay/models.py
@@ -2,13 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+GAME_STATUS_CHOICES = (
+    ("F", "First player to move"),
+    ("S", "Second player to move"),
+    ("W", "First player wins"),
+    ("L", "Second player wins"),
+    ("D", "Draw")
+)
+
+
 class Game(models.Model):
     first_player = models.ForeignKey(User, related_name="games_first_player")
     second_player = models.ForeignKey(User, related_name="games_second_player")
 
     start_time = models.DateTimeField(auto_now_add=True)
     last_active = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1, default="F")
+    status = models.CharField(max_length=1, default="F", choices=GAME_STATUS_CHOICES)
+
+    def __str__(self):
+        return "{0} vs {1}".format(self.first_player, self.second_player)
 
 
 class Move(models.Model):
