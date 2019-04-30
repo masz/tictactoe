be7852507c9fc4d195022d44498a97987bc2b43c
masz <masz@mail.de>
2019-04-30 17:29:26 +0200

    login and logout

--- player/views.py
+++ player/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from gameplay.models import Game
 
 
+@login_required
 def home(request):
     all_my_games = Game.objects.games_for_user(request.user)
     active_games = all_my_games.active()
